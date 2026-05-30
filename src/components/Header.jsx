9477318d80839363c56dbcd2cb717a7bb3025fb6
milan <milanzivanov@users.noreply.github.com>
2026-05-30 18:12:51 +0200

    Add Button component and currency formatting utility
    - Introduced a reusable Button component for consistent button styling.
    - Updated Header and MealItem components to use the new Button component.
    - Added currencyFormatter utility for formatting meal prices.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
 
 function Header() {
   return (
@@ -8,7 +9,7 @@ function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly={true}>Cart (0)</Button>
       </nav>
     </header>
   );
